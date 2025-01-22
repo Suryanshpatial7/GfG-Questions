8247220bf8292db01fdde4823c4ec404aacbded3
Suryanshpatial7 <130823312+Suryanshpatial7@users.noreply.github.com>
2025-01-22 22:21:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Subset/array-subset.py
@@ -0,0 +1,42 @@
+#User function Template for python3
+
+class Solution:
+    #Function to check if a is a subset of b.
+    def isSubset(self, a, b):
+       set1 = set(a)
+       set2 = set(b)
+       
+       return set2.issubset(set1)
+    
+    
+    
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+
+    T = int(input())
+
+    while (T > 0):
+        a1 = [int(x) for x in input().strip().split()]
+        a2 = [int(x) for x in input().strip().split()]
+        ob = Solution()
+        if ob.isSubset(a1, a2):
+            print("true")
+        else:
+            print("false")
+
+        T -= 1
+
+        print("~")
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
